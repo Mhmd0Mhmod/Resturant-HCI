50eacc2b7cf57e99fef00ec10dcd4c780d3592b9
Youssef Shaheen <shaheeny616@gmail.com>
2024-12-18 18:44:41 +0200

    Finish header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
+import { AnimatePresence } from "motion/react";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CreateContainer, Header, MainContainer } from "./components";
-import { AnimatePresence } from "motion/react";
-import { Provider } from "react-redux";
 import { store } from "./context/store";
-import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <AnimatePresence>
+        <AnimatePresence mode="wait">
           <div className="flex h-auto w-screen flex-col bg-primary">
             <Header />
-            <main className="mt-24 w-full p-8">
+            <main className="mt-16 md:mt-24 w-full p-8">
               <Routes>
                 <Route path="/*" element={<MainContainer />} />
                 <Route path="/createItem" element={<CreateContainer />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
+import { motion } from "motion/react";
+import { useEffect, useState } from "react";
+import { MdAdd, MdLogout, MdShoppingBasket } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { setUser } from "../context/slice";
+import { signInWithGoogle } from "../DB/services";
+import { supabase } from "../DB/Supabase";
 import Avatar from "./../imgs/avatar.png";
 import Logo from "./../imgs/logo.png";
-import { motion } from "motion/react";
-import { Link } from "react-router-dom";
-import { signInWithGoogle } from "../DB/services";
-import { useEffect, useState } from "react";
-import { supabase } from "../DB/Supabase";
-import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "../context/slice";
 const AdminsID = ["c8b289b4-6374-4419-a259-d9a356ea7aec"];
 function Header() {
   const dispatch = useDispatch();
@@ -34,7 +34,7 @@ function Header() {
   const id = user?.id;
   const isAdmin = AdminsID.includes(id);
   return (
-    <header className="fixed z-50 w-screen p-6 px-16">
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
       {/* desktop and tablet size */}
       <div className="hidden h-full w-full items-center justify-between md:flex">
         <Link to={"/"} className="flex items-center gap-2">
@@ -43,7 +43,10 @@ function Header() {
         </Link>
 
         <div className="flex items-center gap-8">
-          <ul className="flex items-center gap-8">
+          <motion.ul initial={{ opacity: 0, x: 200 }} 
+            animate={{ opacity: 1, x: 0 }} 
+            exit={{ opacity: 0, x: 200 }}
+            className="flex items-center gap-8">
             <li className="cursor-pointer text-base text-textColor transition-all duration-100 ease-in-out hover:text-headingColor">
               Home
             </li>
@@ -56,7 +59,7 @@ function Header() {
             <li className="cursor-pointer text-base text-textColor transition-all duration-100 ease-in-out hover:text-headingColor">
               Service
             </li>
-          </ul>
+          </motion.ul>
 
           <div className="relative flex items-center justify-center">
             <MdShoppingBasket className="cursor-pointer text-2xl text-textColor" />
@@ -73,7 +76,10 @@ function Header() {
               onClick={login}
             />
             {isMenu && (
-              <div className="absolute right-0 top-12 flex w-40 flex-col rounded-lg bg-primary shadow-xl">
+              <motion.div initial={{ opacity: 0, scale: 0.6 }}
+               animate={{ opacity: 1, scale: 1 }}
+               exit={{ opacity: 0, scale: 0.6 }}
+               className="absolute right-0 top-12 flex w-40 flex-col rounded-lg bg-primary shadow-xl">
                 {isAdmin && (
                   <Link to={"/createItem"}>
                     <p className="flex cursor-pointer items-center gap-3 px-4 py-2 text-base text-textColor transition-all duration-100 ease-in-out hover:bg-slate-100">
@@ -81,17 +87,77 @@ function Header() {
                     </p>
                   </Link>
                 )}
-                <p className="flex cursor-pointer items-center gap-3 px-4 py-2 text-base text-textColor transition-all duration-100 ease-in-out hover:bg-slate-100">
+                <p className="flex cursor-pointer items-center gap-3 px-4 py-2 text-base text-textColor transition-all duration-100 ease-in-out hover:bg-slate-100"
+                // onClick={logout}
+                >
                   Logout <MdLogout />
                 </p>
-              </div>
+              </motion.div>
             )}
           </div>
         </div>
       </div>
 
       {/* mobile size */}
-      <div className="flex h-full w-full md:hidden"></div>
+      <div className="flex items-center justify-between h-full w-full md:hidden">
+        
+
+        <div className="relative flex items-center justify-center">
+            <MdShoppingBasket className="cursor-pointer text-2xl text-textColor" />
+            <div className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-cartNumBg">
+              <p className="text-xs font-semibold text-white">2</p>
+            </div>
+        </div>
+
+        <Link to={"/"} className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="Logo" />
+          <p className="text-xl font-bold text-headingColor">City</p>
+        </Link>
+        
+        <div className="relative">
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              src={avatar_url}
+              alt="user"
+              className="h-10 min-h-[40px] w-10 min-w-[40px] cursor-pointer rounded-full drop-shadow-xl"
+              onClick={login}
+            />
+            {isMenu && (
+              <motion.div initial={{ opacity: 0, scale: 0.6 }}
+               animate={{ opacity: 1, scale: 1 }}
+               exit={{ opacity: 0, scale: 0.6 }}
+               className="absolute right-0 top-12 flex w-40 flex-col rounded-lg bg-primary shadow-xl">
+                {isAdmin && (
+                  <Link to={"/createItem"}>
+                    <p className="flex cursor-pointer items-center gap-3 px-4 py-2 text-base text-textColor transition-all duration-100 ease-in-out hover:bg-slate-100">
+                      New Item <MdAdd />
+                    </p>
+                  </Link>
+                )}
+                <ul 
+                    className="flex flex-col">
+                    <li className="hover:bg-slate-100 px-4 py-2 cursor-pointer text-base text-textColor transition-all duration-100 ease-in-out hover:text-headingColor">
+                      Home
+                    </li>
+                    <li className="hover:bg-slate-100 px-4 py-2 cursor-pointer text-base text-textColor transition-all duration-100 ease-in-out hover:text-headingColor">
+                      Menu
+                    </li>
+                    <li className="hover:bg-slate-100 px-4 py-2 cursor-pointer text-base text-textColor transition-all duration-100 ease-in-out hover:text-headingColor">
+                      About Us
+                    </li>
+                    <li className="hover:bg-slate-100 px-4 py-2 cursor-pointer text-base text-textColor transition-all duration-100 ease-in-out hover:text-headingColor">
+                      Service
+                    </li>
+                </ul>
+                <p className="flex cursor-pointer items-center justify-center bg-gray-200 gap-3 m-2 p-2 rounded-md shadow-md text-base text-textColor transition-all duration-100 ease-in-out hover:bg-gray-300"
+                  // onClick={logout}
+                >
+                  Logout <MdLogout />
+                </p>
+              </motion.div>
+            )}
+          </div>
+      </div>
     </header>
   );
 }
