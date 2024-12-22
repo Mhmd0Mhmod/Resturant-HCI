5ad7ee8a8adc510ac80d94f42756a105e40e651d
Yazed Elkholy <yazedelkholy2003@gmail.com>
2024-12-22 19:37:33 +0200

    Loader Component "Tailwind CSS Spinner"

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,5 @@ export { default as MainContainer } from "./MainContainer";
 export { default as CreateContainer } from "./CreateContainer";
 
 export { default as HomContainer } from "./HomeContainer";
+
+export { default as Loader } from "./Loader";
